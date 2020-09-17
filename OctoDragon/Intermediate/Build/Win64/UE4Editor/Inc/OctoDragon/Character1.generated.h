54c7daccf8a655933d205ff009cafbeb245251fb
Shrinath Rao <shrinathrao97@gmail.com>
2020-09-16 22:03:07 -0500

    Basic xy movement working

---
+++ OctoDragon/Intermediate/Build/Win64/UE4Editor/Inc/OctoDragon/Character1.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef OCTODRAGON_Character1_generated_h
+#error "Character1.generated.h already included, missing '#pragma once' in Character1.h"
+#endif
+#define OCTODRAGON_Character1_generated_h
+
+#define OctoDragon_Source_OctoDragon_Character1_h_12_SPARSE_DATA
+#define OctoDragon_Source_OctoDragon_Character1_h_12_RPC_WRAPPERS
+#define OctoDragon_Source_OctoDragon_Character1_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define OctoDragon_Source_OctoDragon_Character1_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACharacter1(); \
+	friend struct Z_Construct_UClass_ACharacter1_Statics; \
+public: \
+	DECLARE_CLASS(ACharacter1, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/OctoDragon"), NO_API) \
+	DECLARE_SERIALIZER(ACharacter1)
+
+
+#define OctoDragon_Source_OctoDragon_Character1_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesACharacter1(); \
+	friend struct Z_Construct_UClass_ACharacter1_Statics; \
+public: \
+	DECLARE_CLASS(ACharacter1, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/OctoDragon"), NO_API) \
+	DECLARE_SERIALIZER(ACharacter1)
+
+
+#define OctoDragon_Source_OctoDragon_Character1_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACharacter1(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACharacter1) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACharacter1); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACharacter1); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACharacter1(ACharacter1&&); \
+	NO_API ACharacter1(const ACharacter1&); \
+public:
+
+
+#define OctoDragon_Source_OctoDragon_Character1_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACharacter1(ACharacter1&&); \
+	NO_API ACharacter1(const ACharacter1&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACharacter1); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACharacter1); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ACharacter1)
+
+
+#define OctoDragon_Source_OctoDragon_Character1_h_12_PRIVATE_PROPERTY_OFFSET
+#define OctoDragon_Source_OctoDragon_Character1_h_9_PROLOG
+#define OctoDragon_Source_OctoDragon_Character1_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	OctoDragon_Source_OctoDragon_Character1_h_12_PRIVATE_PROPERTY_OFFSET \
+	OctoDragon_Source_OctoDragon_Character1_h_12_SPARSE_DATA \
+	OctoDragon_Source_OctoDragon_Character1_h_12_RPC_WRAPPERS \
+	OctoDragon_Source_OctoDragon_Character1_h_12_INCLASS \
+	OctoDragon_Source_OctoDragon_Character1_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define OctoDragon_Source_OctoDragon_Character1_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	OctoDragon_Source_OctoDragon_Character1_h_12_PRIVATE_PROPERTY_OFFSET \
+	OctoDragon_Source_OctoDragon_Character1_h_12_SPARSE_DATA \
+	OctoDragon_Source_OctoDragon_Character1_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	OctoDragon_Source_OctoDragon_Character1_h_12_INCLASS_NO_PURE_DECLS \
+	OctoDragon_Source_OctoDragon_Character1_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> OCTODRAGON_API UClass* StaticClass<class ACharacter1>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID OctoDragon_Source_OctoDragon_Character1_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
